bdf578acf796c1eaaeb562d46228b632d0414255
Julian Ferres <julianferres@gmail.com>
2022-01-12 17:40:03 -0300

    Responsive title

---
+++ src/ConditionalTerminalHeader.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { FaGithubSquare } from "react-icons/fa";
+import NavButtons from "./NavButtons";
+import "./Terminal.css";
+
+const GITHUB = "https://github.com/julianferres";
+const GITHUB_SHORT = "julianferres";
+
+const compactTitle = { animation: "titleAnimationCompact 500ms ease" };
+const fullSizeTitle = { animation: "titleAnimationFullSize 500ms ease" };
+
+export default function ConditionalTerminalHeader() {
+  /* Manage screen width changes */
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const breakpoint = 700;
+  React.useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+    // subscribe to window resize event "onComponentDidMount"
+    window.addEventListener("resize", handleResizeWindow);
+    return () => {
+      // unsubscribe "onComponentDestroy"
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
+
+  const compact = width < breakpoint;
+
+  return (
+    <div className="terminal-header">
+      <NavButtons className="buttons" />
+      <h1 className="title" style={compact ? compactTitle : fullSizeTitle}>
+        <FaGithubSquare style={{ fontSize: "14px", fontWeight: 400 }} />
+        <a className="title-href" href={GITHUB}>
+          {compact ? GITHUB_SHORT : GITHUB}
+        </a>
+      </h1>
+    </div>
+  );
+}
--- src/Terminal.js
+++ src/Terminal.js
@@ -1,20 +1,10 @@
-import NavButtons from "./NavButtons";
-import { FaGithubSquare } from "react-icons/fa";
 import "./Terminal.css";
+import ConditionalTerminalHeader from "./ConditionalTerminalHeader";
 
 export default function Terminal() {
   return (
     <div className="terminal">
-      <div className="terminal-header">
-        <NavButtons className="buttons" />
-        <h1 className="title">
-          <FaGithubSquare style={{ fontSize: "14px", fontWeight: 400 }} />
-
-          <a className="title-href" href="https://github.com/julianferres">
-            https://github.com/julianferres
-          </a>
-        </h1>
-      </div>
+      <ConditionalTerminalHeader />
       <div className="terminal-body">
         <p>Welcome to Julian Ferres' webpage</p>
         <p>Welcome to Julian Ferres' webpage</p>
